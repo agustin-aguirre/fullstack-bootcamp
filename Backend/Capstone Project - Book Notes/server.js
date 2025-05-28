5adfb9dcb0b56d170190036f5f0e1adf3e9ceb46
Agustín <agus.aguirre.pro@gmail.com>
2025-05-28 18:35:19 -0300

    Started CAPSTONE PROJECT add postgres db connection

---
+++ Backend/Capstone Project - Book Notes/server.js
@@ -0,0 +1,25 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import express from "express";
+import { getAll } from "./source/repositories/postgressDb.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const port = 3000;
+const app = express();
+
+
+app.use(express.static("public"));
+app.set('views', path.join(__dirname, "source", "views", "pages"));
+
+
+app.get("/", async (req, res) => {
+    const books = await getAll();
+    res.render("index.ejs", {books: books});
+});
+
+
+app.listen(port, () => {
+  console.log(`Server is running at http://localhost:${port}`);
+});
