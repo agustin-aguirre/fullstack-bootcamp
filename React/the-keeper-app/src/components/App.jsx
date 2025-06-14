9c1c19b389b6cbd02457e441e3fb0117ba3798da
Agustín Aguirre <agus.aguirre.pro@gmail.com>
2025-06-14 16:52:39 -0300

    Added components to Keeper app and added components exercises

--- React/the-keeper-app/src/components/App.jsx
+++ React/the-keeper-app/src/components/App.jsx
@@ -2,13 +2,24 @@ import Header from "./Header"
 import Note from "./Note"
 import Footer from "./Footer"
 
+import notes from "../data/notes"
+
+
+function createNote(noteData) {
+  return <Note 
+  key={noteData.key}
+  title={noteData.title}
+  content={noteData.content}
+  />;
+}
+
 
 function App() {
   
   return (
     <>
     <Header />
-    <Note />
+    {notes.map(noteData => createNote(noteData))}
     <Footer />
     </>
   )
--- React/the-keeper-app/src/components/Note.jsx
+++ React/the-keeper-app/src/components/Note.jsx
@@ -1,10 +1,8 @@
-export default function Note() {
-    const title = "This is the note title";
-    const content = "This is the note content"
+export default function Note(props) {
     return (
         <div className="note">
-            <h1>{title}</h1>
-            <p>{content}</p>
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
         </div>
     );
 }
